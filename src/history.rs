bc9a2b156df84b70c944f619e456c35e2ae83e3a
braindaddy73 <heiko@kitscha.de>
2024-02-05 11:23:57 +0100

    Update history.rs

--- src/history.rs
+++ src/history.rs
@@ -1,5 +1,6 @@
 pub struct History {
   history: [f32, 100],
+  mean: f32
 }
 
 impl Data {
@@ -7,16 +8,27 @@ impl Data {
   pub fn clear() {
     for i in 0..99 {
       history[i] = 0;
+    }
+    mean = 0.0;
   }
 
   pub fn update(&mut self, val: f32) {
     shift_left();
     history[99] = val;
+    update_mean();
   }
     
   fn shift_left (&mut self) {
     for i in 1..99 {
       history[i-1] = history [i];
   }
-    
+
+  fn update_mean (&mut self) {
+    buf = 0;
+    for i in 0.99 {
+      buf += history[i];
+    }
+    mean = buf / 100.00;
+  }
+
 }
